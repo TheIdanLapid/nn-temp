2155e841b471a0d0f24e6f1904684d9790d50d4c
DekelSror <srordekel@gmail.com>
2019-01-10 12:18:20 +0200

    first

--- net.py
+++ net.py
@@ -1,69 +1,49 @@
 import numpy as np
-from nlib_dummydata import xor_data
 from nlib_func import sig, dx_sig
-from pprint import pprint as pprint
+from nlib_dummydata import xor_data
 import pdb
 
-class nLayer:
-    def __init__(self, dim):
-        self.values = np.zeros(shape=(dim,1))
+class Rigid:
+    def __init__(self):
+        self.il = np.zeros(shape=(2,1))
+        self.w_ih = np.array([[np.random.random() for i in range(3)] for j in range(2)])
+        self.hl = np.zeros(shape=(3,1))
+        self.w_ho = np.array([[np.random.random() for i in range(1)] for j in range(3)])
+        self.ol = np.zeros(shape=(1,1))
 
+# pdb.set_trace()
 
-class wLayer:
-    def __init__(self, idim, odim):
-        self.w = [[np.random.random() for p in range(odim)] for i in range(idim)]
-
-
-nodes = [nLayer(2), nLayer(3), nLayer(1)]
-weights = [wLayer(2,3), wLayer(3,1)]
-# biases = [np.ones(shape=(2,)), np.ones(shape=(3,)), np.ones(shape=(1,))]
-
-err_arr = []
-
-def one(prt):
+def shum(op=False):
     item = xor_data()
-    nodes[0].values = item['data']
-    nodes[1].values = np.array([sig(j) for j in np.matmul(np.transpose(weights[0].w), nodes[0].values)])
-    nodes[2].values = np.array([sig(n) for n in np.matmul(np.transpose(weights[1].w),  nodes[1].values)])
-    if prt:
-        print('my guess for {} is {}'.format(item['label'], nodes[2].values))
-    out_err = item['label'] - nodes[2].values
-    err_arr.append(out_err)
-    out_grad = np.array([dx_sig(p) for p in nodes[2].values])
-    out_grad = np.matmul(out_grad ,out_err * 0.3)
-    h_err = weights[1].w * out_err
-    h_grad = [dx_sig(p) for p in nodes[1].values]
-    h_grad = np.matmul(h_grad, h_err * 0.3)
-    out_dlt = np.matmul(out_grad, np.transpose(nodes[1].values))
-    pdb.set_trace()
-    h_dlt = np.matmul(h_grad*-1, [nodes[0].values])
-    weights[0].w += h_dlt
-    weights[1].w += out_dlt
 
-i=0
-while i < 1000:
-    if i%100 == 4:
-        one(True)
+    f.il = item['data']
+    f.hl = [sig(x) for x in np.matmul(np.transpose(f.w_ih), f.il)]
+    f.ol = [sig(x) for x in np.matmul(np.transpose(f.w_ho), f.hl)]
+    if op:
+        print('label:{} guess:{}'.format(item['label'], f.ol))
+    guess = f.ol
+    out_err = np.array([abs(l-g) for l,g in zip(item['label'], guess)])
+    out_grad = np.array([dx_sig(p) for p in f.ol])
+    out_grad = np.matmul(out_grad, out_err*0.3)
+
+    h_err = np.matmul(f.w_ho, out_err)
+    h_grad = np.array([dx_sig(p) for p in f.hl])
+    h_grad = np.matmul(h_grad, h_err*0.3)
+
+    ho_dlt = [og*hlt for og, hlt in zip([out_grad], np.transpose(f.hl))]
+    ih_dlt = [hg*ilt for hg, ilt in zip([h_grad], np.transpose(f.il))]
+
+    f.w_ho += ho_dlt
+    f.w_ih += ih_dlt
+
+
+d = 0
+f = Rigid()
+while d < 1000:
+    if d%100 == 4:
+        shum(True)
+    elif d == 999:
+        shum(True)
     else:
-        one(False)
-    i += 1
-
-
-pprint(err_arr[0:10:1])
-print('\n ------------')
-pprint(err_arr[-10:-1:1])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-stopit = 7
+        shum()
+    d += 1
